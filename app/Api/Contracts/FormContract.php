15eb34186d395aeede6f72ebb36331aa2a8f6466
BeauitfullXiaoJian <xiaojiannangkang@126.com>
2018-06-20 15:00:25 +0800

    remove formconcat repeat method

--- app/Api/Contracts/FormContract.php
+++ app/Api/Contracts/FormContract.php
@@ -102,15 +102,6 @@ interface FormContract
     public function camelForm($rules = [], $reback = true);
 
     /**
-     * 校验表单数据,自动转化匹配参数为小驼峰，如要匹配a_b将会变为匹配aB，抛出异常
-     * 
-     * @param array $rules
-     * @param bool $reback 是否需要还原参数原始名称，默认为ture:需要
-     * @return array
-     */
-    public function camelFormOrFail($rules = [], $reback = true);
-
-    /**
      * 获取所有表单数据
      * @return array
      */
